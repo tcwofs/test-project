b6f471b1f76ab998d447d63cbfc9eb70d65083c6
tcwofs <liampicave@gmail.com>
2021-04-13 10:31:11 +0300

    feat: add new routes

---
+++ src/components/Main/Details/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>post {{ $route.params.id }}</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/Main/Post/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    post {{ $route.params.id }} <br />
+    {{ post }}
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  name: 'Post',
+  data: () => ({
+    post: null
+  }),
+  async mounted() {
+    this.post = await this.getPost(this.$route.params.id);
+  },
+  methods: {
+    ...mapActions({
+      getPost: 'global/getPost'
+    })
+  }
+};
+</script>
+
+<style></style>
